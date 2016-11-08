b82187b7425f84a536c00dd9790c2603a136e170
kylethefox <halobuster1982>
2016-11-08 16:49:29 -0500

    Update project to automatically upload and download info from the web service

---
+++ app/src/main/java/com/example/hongear/quiet_lounge03/LocationInfo.java
@@ -0,0 +1,48 @@
+package com.example.hongear.quiet_lounge03;
+
+/**
+ * Created by kyleoneill on 11/7/16.
+ */
+
+public class LocationInfo {
+
+    private double lat;
+    private double lng;
+    private double sound;
+
+    public LocationInfo() {
+        this.lat = 0;
+        this.lng = 0;
+        this.sound = 0;
+    }
+
+    public LocationInfo(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+        this.sound = 0;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public double getSound() {
+        return sound;
+    }
+
+    public void setSound(double sound) {
+        this.sound = sound;
+    }
+}
--- app/src/main/java/com/example/hongear/quiet_lounge03/MainActivity.java
+++ app/src/main/java/com/example/hongear/quiet_lounge03/MainActivity.java
@@ -1,23 +1,228 @@
 package com.example.hongear.quiet_lounge03;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Random;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "DecibelTest";
     public static double REFERENCE = 0.00002;
+    private final static String GET_URL = "http://quietlounge.us-east-1.elasticbeanstalk.com/getLoungeData";
+    private final static String POST_URL = "http://quietlounge.us-east-1.elasticbeanstalk.com/inputSound";
+    private final static int TIME_BETWEEN_HTTP_REQUESTS = 2000;
+    private double sound;
+    private LocationInfo locationInfo;
+    private RequestQueue queue;
+    private JSONObject loungeResponseData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+        locationInfo = new LocationInfo();
+
+        // Runtime permission requests - SDK 23 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
+
+        // Code needed to get Latitude and Longitude coordinates of phone
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                locationInfo.setLat(location.getLatitude());
+                locationInfo.setLng(location.getLongitude());
+                Log.d("Update", "Location Updated");
+                Log.d("New Coordinates", "Lat: " + locationInfo.getLat() + " Lng: " + locationInfo.getLng());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        // Gets the Coordinates from phone, if it has permissions
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        // Create Timer to Constantly send new sound Data
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+//                locationInfo.setSound(getNoiseLevel());
+                queue.add(insertSoundData(locationInfo));        // Use with emulator
+                queue.add(getLoungeData());
+            }
+        }, new Date(), TIME_BETWEEN_HTTP_REQUESTS);
+    }
+
+
+    public void refreshData(View view) {
+        Log.d("get Data", "Pressed Refresh");
+        queue.add(getLoungeData());
+    }
+
+    public void heatMap(View view) {
+        Toast.makeText(this, "switching view...", Toast.LENGTH_SHORT).show();
+        Intent homeIntent = new Intent(MainActivity.this, HeatMap.class);
+        startActivity(homeIntent);
+        finish();
+    }
+
+    public JsonObjectRequest insertSoundData(final LocationInfo local) {
+
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, POST_URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String responseMsg = response.getString("msg");
+                            Log.d("API Response", "Response Message: " + responseMsg);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        })
+        {
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    String bodyStr;
+//                    bodyStr = "lat=" + local.getLat() + "&lng=" + local.getLng() + "&sound=" + local.getSound();
+                    bodyStr = "lat=" + local.getLat() + "&lng=" + local.getLng() + "&sound=" + String.valueOf(Math.random() * 30);
+                    return bodyStr.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
+
+        return request;
+    }
+
+    @SuppressWarnings("unused")
+    public JsonObjectRequest getLoungeData() {
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, GET_URL,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                    JSONArray dataJsonArray = response.getJSONArray("lounges");
+                    JSONObject data;
+                    TextView textView;
+                    String soundLevel;
+                    DecimalFormat df = new DecimalFormat("#.###");
+
+                    // Update Data for SERC [0]
+                    data = dataJsonArray.getJSONObject(0);
+                    textView = (TextView) findViewById(R.id.serc_data);
+                    soundLevel = df.format(data.getDouble("lastSoundLevel"));
+                    textView.setText(soundLevel);
+
+                    // Update Data for Tech Center [1]
+                    data = dataJsonArray.getJSONObject(1);
+                    textView = (TextView) findViewById(R.id.tech_data);
+                    soundLevel = df.format(data.getDouble("lastSoundLevel"));
+                    textView.setText(soundLevel);
+
+                    // Update Data for Student Center [2]
+                    data = dataJsonArray.getJSONObject(2);
+                    textView = (TextView) findViewById(R.id.student_center_data);
+                    soundLevel = df.format(data.getDouble("lastSoundLevel"));
+                    textView.setText(soundLevel);
+
+                    // Update Data for Library [3]
+                    data = dataJsonArray.getJSONObject(3);
+                    textView = (TextView) findViewById(R.id.library_data);
+                    soundLevel = df.format(data.getDouble("lastSoundLevel"));
+                    textView.setText(soundLevel);
+
+                    // Update Data for Wachman Hall [4]
+                    data = dataJsonArray.getJSONObject(4);
+                    textView = (TextView) findViewById(R.id.wachman_data);
+                    soundLevel = df.format(data.getDouble("lastSoundLevel"));
+                    textView.setText(soundLevel);
+
+                    Log.d("Update Sound levels","Updated");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        return getRequest;
+    }
 
     public double getNoiseLevel() {
         Log.e(TAG, "start new recording process");
@@ -63,86 +268,5 @@ public class MainActivity extends AppCompatActivity {
             return 0;
         }
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void addData(View view) {
-        Toast.makeText(this, "capture and send data to db", Toast.LENGTH_SHORT).show();
-        //double db = getNoiseLevel();
-        //// TODO: 10/25/2016 send decibel level to manager with post request for storage
-
-    }
-
-
-    public void refreshData(View view) {
-        //Toast.makeText(this, "refreshed...", Toast.LENGTH_LONG).show();
-
-        ///// TODO: 10/25/2016 send get request to manager for data to populate table
-
-        Random r = new Random();
-        int rand = r.nextInt(69 - 28) + 28;
-        String random = Integer.toString(rand);
-
-        TextView tv = (TextView) findViewById(R.id.row2_tv2);
-        tv.setText(random);
-        if (rand > 45) {
-            tv.setBackgroundColor(Color.RED);
-        } else {
-            tv.setBackgroundColor(Color.GREEN);
-        }
-
-        rand = r.nextInt(52 - 28) + 28;
-        random = Integer.toString(rand);
-        tv = (TextView) findViewById(R.id.row3_tv2);
-        tv.setText(random);
-        if (rand > 45) {
-            tv.setBackgroundColor(Color.RED);
-        } else {
-            tv.setBackgroundColor(Color.GREEN);
-        }
-
-        rand = r.nextInt(56 - 28) + 28;
-        random = Integer.toString(rand);
-        tv = (TextView) findViewById(R.id.row4_tv2);
-        tv.setText(random);
-        if (rand > 45) {
-            tv.setBackgroundColor(Color.RED);
-        } else {
-            tv.setBackgroundColor(Color.GREEN);
-        }
-
-        rand = r.nextInt(46 - 28) + 28;
-        random = Integer.toString(rand);
-        tv = (TextView) findViewById(R.id.row5_tv2);
-        tv.setText(random);
-        if (rand > 45) {
-            tv.setBackgroundColor(Color.RED);
-        } else {
-            tv.setBackgroundColor(Color.GREEN);
-        }
-
-        rand = r.nextInt(49 - 28) + 28;
-        random = Integer.toString(rand);
-        tv = (TextView) findViewById(R.id.row6_tv2);
-        tv.setText(random);
-        if (rand > 45) {
-            tv.setBackgroundColor(Color.RED);
-        } else {
-            tv.setBackgroundColor(Color.GREEN);
-        }
-
-
-    }
-
-    public void heatMap(View view) {
-        Toast.makeText(this, "switching view...", Toast.LENGTH_SHORT).show();
-        Intent homeIntent = new Intent(MainActivity.this, HeatMap.class);
-        startActivity(homeIntent);
-        finish();
     }
 }
